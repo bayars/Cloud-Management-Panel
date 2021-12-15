bdc27a2f051916d6e28307c754a46a3537bf37ff
Safa Bayar <rectionxx@gmail.com>
2021-12-14 22:44:32 -0500

    Updated django and added postgres docker image

--- cloudpanel/urls.py
+++ cloudpanel/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 import explorer.views
@@ -6,20 +6,20 @@ import manager.views
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$',explorer.views.home,name='home'),
-    url(r'^servers/$', explorer.views.servers),
-    url(r'^addserver/$', explorer.views.addserver),
-    url(r'^removeserver/$', explorer.views.removeserver),
-    url(r'^manage/(?P<server>[\w]+)/(?P<path>[\w]+)/$', manager.views.filemanager),
-    url(r'^navback/(?P<server>[\w]+)/(?P<path>[\*\w]+)/$', manager.views.navbackward),
-    url(r'^naviforward/(?P<server>[\w]+)/(?P<path>[\*\w]+)/(?P<dir>[\w]+)/$', manager.views.navforward),
-    url(r'^edit/(?P<server>[\w]+)/(?P<path>[\*\w]+)/(?P<file>[\.\w]+)$', manager.views.editdata),
-    url(r'^saveandsend/(?P<server>[\w]+)/(?P<path>[\*\w]+)/(?P<file>[\.\w]+)$', manager.views.senddata),
-    url(r'^upload/(?P<server>[\w]+)/(?P<path>[\*\w]+)$', manager.views.uploadfile),
-    url(r'^delete/(?P<server>[\w]+)/(?P<path>[\*\w]+)/(?P<file>[\.\w]+)$', manager.views.deletefile),
-	#url(r'^servers/$','explorer.views.servers'),
-    #url(r'^servers/$','explorer.views.servers'),/\
+    path(r'^admin/', admin.site.urls),
+    path(r'^$',explorer.views.home,name='home'),
+    path(r'^servers/$', explorer.views.servers),
+    path(r'^addserver/$', explorer.views.addserver),
+    path(r'^removeserver/$', explorer.views.removeserver),
+    path(r'^manage/(?P<server>[\w]+)/(?P<path>[\w]+)/$', manager.views.filemanager),
+    path(r'^navback/(?P<server>[\w]+)/(?P<path>[\*\w]+)/$', manager.views.navbackward),
+    path(r'^naviforward/(?P<server>[\w]+)/(?P<path>[\*\w]+)/(?P<dir>[\w]+)/$', manager.views.navforward),
+    path(r'^edit/(?P<server>[\w]+)/(?P<path>[\*\w]+)/(?P<file>[\.\w]+)$', manager.views.editdata),
+    path(r'^saveandsend/(?P<server>[\w]+)/(?P<path>[\*\w]+)/(?P<file>[\.\w]+)$', manager.views.senddata),
+    path(r'^upload/(?P<server>[\w]+)/(?P<path>[\*\w]+)$', manager.views.uploadfile),
+    path(r'^delete/(?P<server>[\w]+)/(?P<path>[\*\w]+)/(?P<file>[\.\w]+)$', manager.views.deletefile),
+	#path(r'^servers/$','explorer.views.servers'),
+    #path(r'^servers/$','explorer.views.servers'),/\
 
 
 ]
